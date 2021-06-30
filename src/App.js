72428a561be04c58c4cfd5e01eb77d8279e0398d
Basith <mdbasith635@gmail.com>
2021-06-30 12:40:29 +0530

    formatted

--- src/App.js
+++ src/App.js
@@ -6,21 +6,18 @@ import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 import CartProvider from './store/CartProvider';
 
-const App = () =>
-{
-  const [ showCart, setShowCart ] = useState( false );
-  const showCartHandler = () =>
-  {
-    setShowCart( true );
+const App = () => {
+  const [showCart, setShowCart] = useState(false);
+  const showCartHandler = () => {
+    setShowCart(true);
   };
-  const hideCartHandler = () =>
-  {
-    setShowCart( false );
+  const hideCartHandler = () => {
+    setShowCart(false);
   };
   return (
     <CartProvider>
-      { showCart && <Cart onClose={ hideCartHandler } /> }
-      <Header onShowCart={ showCartHandler } />
+      {showCart && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <section>
         <Meals />
       </section>
